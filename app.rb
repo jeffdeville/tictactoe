5e90408c364877677ede652dabde6d5fbe2130a6
Jeff Deville <jeffdeville@gmail.com>
2012-02-08 15:21:27 -0500

    created a rules object that encapsulates valid moves and handles scoring. Relies on the Board object

--- app.rb
+++ app.rb
@@ -22,50 +22,63 @@ class Board
   end
 end
 
-board = Board.new
+class Rules
+  @@left_diagonal = [[0, 0], [1, 1], [2, 2]]
+  @@right_diagonal = [[2, 0], [1, 1], [0, 2]]
 
-left_diagonal = [[0, 0], [1, 1], [2, 2]]
-right_diagonal = [[2, 0], [1, 1], [0, 2]]
+  def initialize(board)
+    @board = board
+  end
+
+  def valid_move?(row, col)
+    begin
+      @board.fetch(row, col)
+    rescue IndexError
+      puts "Out of bounds, try another position"
+      return false
+    end
+
+    if @board.fetch(row, col)
+      puts "Cell occupied, try another position"
+      return false
+    end
+    true
+  end
+
+  def winner?(row, col, current_player)
+    lines = []
+
+    [@@left_diagonal, @@right_diagonal].each do |line|
+      lines << line if line.include?([row, col])
+    end
+
+    lines << (0..2).map { |c1| [row, c1] }
+    lines << (0..2).map { |r1| [r1, col] }
+
+    lines.any? do |line|
+      line.all? { |row, col| @board.fetch(row, col) == current_player }
+    end
+  end
+end
+
+board = Board.new
+rules = Rules.new board
 
 players = [:X, :O].cycle
 
 current_player = players.next
 
 loop do
-  puts board.to_s # map { |row| row.map { |e| e || " " }.join("|") }.join("\n")
+  puts board.to_s
   print "\n>> "
   row, col = gets.split.map { |e| e.to_i }
   puts
 
-  begin
-    cell_contents = board.fetch(row, col)
-  rescue IndexError
-    puts "Out of bounds, try another position"
-    next
-  end
-
-  if cell_contents
-    puts "Cell occupied, try another position"
-    next
-  end
+  next unless rules.valid_move? row, col
 
   board.move row, col, current_player
-  #board[row][col] = current_player
 
-  lines = []
-
-  [left_diagonal, right_diagonal].each do |line|
-    lines << line if line.include?([row, col])
-  end
-
-  lines << (0..2).map { |c1| [row, c1] }
-  lines << (0..2).map { |r1| [r1, col] }
-
-  win = lines.any? do |line|
-    line.all? { |row, col| board.fetch(row, col) == current_player }
-  end
-
-  if win
+  if rules.winner? row, col, current_player
     puts "#{current_player} wins!"
     exit
   end
