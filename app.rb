785283d0504ca2a2f37b89b79c2dbd5f438421f4
Jeff Deville <jeffdeville@gmail.com>
2012-02-08 17:48:43 -0500

    final cleanup, only problem is the need to check wins and draws. Board should be able to use callbacks for this, I feel

--- app.rb
+++ app.rb
@@ -1,106 +1,100 @@
-class Board
-  def initialize
-    @board = [[nil, nil, nil],
-              [nil, nil, nil],
-              [nil, nil, nil]]
-  end
-
-  def to_s
-    puts @board.map { |row| row.map { |e| e || " " }.join("|") }.join("\n")
-  end
-
-  def fetch(x, y)
-    @board[x][y]
-  end
-
-  def move(x, y, player)
-    @board[x][y] = player
-  end
-
-  def draw?
-    @board.flatten.compact.length == 9
-  end
-end
-
-class Rules
-  @@left_diagonal = [[0, 0], [1, 1], [2, 2]]
-  @@right_diagonal = [[2, 0], [1, 1], [0, 2]]
-
-  def initialize(board)
-    @board = board
-  end
-
-  def valid_move?(row, col)
-    begin
-      @board.fetch(row, col)
-    rescue IndexError
-      puts "Out of bounds, try another position"
-      return false
+module TicTacToe
+  class Board
+    class InvalidMove < StandardError;
     end
 
-    if @board.fetch(row, col)
-      puts "Cell occupied, try another position"
-      return false
-    end
-    true
-  end
+    LEFT_DIAGONAL = [[0, 0], [1, 1], [2, 2]]
+    RIGHT_DIAGONAL = [[2, 0], [1, 1], [0, 2]]
 
-  def winner?(row, col, current_player)
-    lines = []
-
-    [@@left_diagonal, @@right_diagonal].each do |line|
-      lines << line if line.include?([row, col])
+    def initialize
+      @board = [[nil, nil, nil],
+                [nil, nil, nil],
+                [nil, nil, nil]]
+      @errors = []
     end
 
-    lines << (0..2).map { |c1| [row, c1] }
-    lines << (0..2).map { |r1| [r1, col] }
-
-    lines.any? do |line|
-      line.all? { |row, col| @board.fetch(row, col) == current_player }
+    def to_s
+      puts @board.map { |row| row.map { |e| e || " " }.join("|") }.join("\n")
     end
-  end
-end
 
-class TicTacToe
-  def initialize
-    @board = Board.new
-    @rules = Rules.new @board
-    @players = [:X, :O].cycle
-    @current_player = @players.next
-  end
+    def [](row, col)
+      @board.fetch(row).fetch(col)
+    end
 
-  def play
-    loop do
-      puts @board.to_s
-      row, col = capture_move
-      #print "\n>> "
-      #row, col = gets.split.map { |e| e.to_i }
-      #puts
+    def []=(row, col, marker)
+      assert_valid_move row, col
+      @board[row][col] = marker
+    end
 
-      next unless @rules.valid_move? row, col
+    def draw?
+      draw = @board.flatten.compact.length == 9
+      yield if draw
+      draw
+    end
 
-      @board.move row, col, @current_player
+    def assert_valid_move(row, col)
+      begin
+        self[row, col]
+      rescue IndexError
+        raise InvalidMove, "Out of bounds, try another position"
+      end
+      raise InvalidMove, "Cell occupied, try another position" if self[row, col]
+    end
 
-      if @rules.winner? row, col, @current_player
-        puts "#{@current_player} wins!"
-        exit
+    def winner?(row, col, current_player)
+      win = intersecting_lines(row, col).any? do |line|
+        line.all? { |row, col| self[row, col] == current_player }
+      end
+      yield if win
+      win
+    end
+
+    def intersecting_lines(row, col)
+      lines = []
+
+      [LEFT_DIAGONAL, RIGHT_DIAGONAL].each do |line|
+        lines << line if line.include?([row, col])
       end
 
-      if @board.draw?
-        puts "It's a draw!"
-        exit
-      end
+      lines << (0..2).map { |c1| [row, c1] }
+      lines << (0..2).map { |r1| [r1, col] }
+      lines
+    end
+  end
 
+  class Game
+    def initialize
+      @board = Board.new
+      @players = [:X, :O].cycle
       @current_player = @players.next
     end
-  end
 
-  def capture_move
-    print "\n>> "
-    row, col = gets.split.map { |e| e.to_i }
-    puts
-    [row, col]
+    def play
+      loop do
+        row, col = capture_move { |error| puts error }
+        exit if @board.winner?(row, col, @current_player) { puts "#{@current_player} wins!" }
+        exit if @board.draw? { puts "It's a draw!'" }
+        @current_player = @players.next
+      end
+    end
+
+    def display_board
+      puts @board.to_s
+    end
+
+    def capture_move
+      display_board
+      print "\n>> "
+      row, col = gets.split.map { |e| e.to_i }
+      puts
+
+      @board[row, col] = @current_player
+      [row, col]
+    rescue TicTacToe::Board::InvalidMove => error
+      yield error.message if block_given?
+      retry
+    end
   end
 end
 
-TicTacToe.new.play
+TicTacToe::Game.new.play
